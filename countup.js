b597d111468b2709f0daead6d63525e7832cd9e4
Régis RIGAUD <thecause@free.fr>
2017-05-14 12:23:11 +0200

    Update countup.js

--- countup.js
+++ countup.js
@@ -2,7 +2,6 @@ var elements = document.getElementById('s');
 var elementm = document.getElementById('m');
 var elementh = document.getElementById('h');
 var elementd = document.getElementById('d');
-
 var seconds = new ProgressBar.Circle(elements, {
     duration: 200,
     color: "#DC242F",
@@ -31,8 +30,7 @@ var days = new ProgressBar.Circle(elementd, {
     strokeWidth: 5,
     trailWidth: 3
 });
-
-shortcode_date = '2016-04-11T11:00:00'
+shortcode_date = '2017-05-08T20:00:00'
  
 setInterval(function() {
     now = new Date(); 
@@ -40,7 +38,7 @@ setInterval(function() {
 	difference = (now-countTo);
     var second = Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
     seconds.animate(second / 60, function() {
-        seconds.setText("<span class=\"number\">" + second + "</span>" + "<span class=\"label\">Seconds</span>");
+        seconds.setText("<span class=\"number\">" + second + "</span>" + "<span class=\"label\">Secondes</span>");
     });
 }, 1000);
 setInterval(function() {
@@ -58,7 +56,7 @@ setInterval(function() {
 	difference = (now-countTo);
     var hour = Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
     hours.animate(hour / 24, function() {
-        hours.setText("<span class=\"number\">" + hour + "</span>" + "<span class=\"label\">Hours</span>");
+        hours.setText("<span class=\"number\">" + hour + "</span>" + "<span class=\"label\">Heures</span>");
     });
 }, 1000);
 setInterval(function() {
@@ -67,6 +65,6 @@ setInterval(function() {
 	difference = (now-countTo);
     var day = Math.floor(difference/(60*60*1000*24)*1);
 	days.animate(day / (day + 5), function() {
-        days.setText("<span class=\"number\">" + day + "</span>" + "<span class=\"label\">Days</span>");
+        days.setText("<span class=\"number\">" + day + "</span>" + "<span class=\"label\">Jours</span>");
     });
 }, 1000);
